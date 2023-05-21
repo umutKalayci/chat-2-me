820f76cf79ba21de932a01d0b57bb7444218ff40
Umut Kalayci <kalayciumut@windowslive.com>
2023-05-21 22:50:05 +0300

    improving:chat page and added chat-box component

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { ChatPageRoutingModule } from './chat-routing.module';
 
 import { ChatPage } from './chat.page';
+import { ChatBoxComponent } from '../components/chat-box/chat-box.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ChatPageRoutingModule
-  ],
-  declarations: [ChatPage]
+  imports: [CommonModule, FormsModule, IonicModule, ChatPageRoutingModule],
+  declarations: [ChatPage, ChatBoxComponent],
 })
 export class ChatPageModule {}
--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Person } from '../discover/IPerson';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +8,37 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  person: Person = {
+    id: 1,
+    name: 'Name 1',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, temporibus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, temporibus.',
+    images: [
+      'https://ionicframework.com/docs/img/demos/card-media.png',
+      'https://ionicframework.com/docs/img/demos/card-media.png',
+      'https://ionicframework.com/docs/img/demos/card-media.png',
+    ],
+  };
+  name: string = 'Sender';
+  message: any;
+  isLoading = false;
+  currentUserId = 1;
+  chats = [
+    { id: 1, sender: 1, message: 'hi' },
+    { id: 2, sender: 2, message: 'hi there!' },
+  ];
+
   constructor(private route: ActivatedRoute) {
     console.log(this.route.snapshot.paramMap.get('id'));
   }
 
   ngOnInit() {}
+
+  sendMessage() {
+    this.chats.push({
+      id: this.chats.length + 1,
+      sender: 1,
+      message: this.message,
+    });
+  }
 }
