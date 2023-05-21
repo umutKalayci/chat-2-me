05faadd255459d24a2c2811c319dd2c00de28570
Umut Kalayci <kalayciumut@windowslive.com>
2023-05-21 04:02:03 +0300

    feat: discover page

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,20 +1,21 @@
 import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-
+import { register } from 'swiper/element/bundle';
+register();
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  items = [
+    {
+      name: 'Name 1',
+      description:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, temporibus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, temporibus.',
+      images: [
+        'https://ionicframework.com/docs/img/demos/card-media.png',
+        'https://ionicframework.com/docs/img/demos/card-media.png',
+        'https://ionicframework.com/docs/img/demos/card-media.png',
+      ],
+    },
+    {
+      name: 'Name 2',
+      description: 'Hello guys.',
+      images: [
+        'https://ionicframework.com/docs/img/demos/card-media.png',
+        'https://ionicframework.com/docs/img/demos/card-media.png',
+      ],
+    },
+    {
+      name: 'Name 3',
+      description: 'Hello guys.',
+      images: ['https://ionicframework.com/docs/img/demos/card-media.png'],
+    },
+  ];
+  selectedItem;
+  card: any;
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+  constructor() {
+    this.selectedItem = this.items[0];
+  }
+  ngOnInit(): void {
+    this.card = document.getElementById('itemCard');
+  }
+  swiperReady() {
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
 
-  constructor() {}
-
+  handleRefresh(event: any) {
+    setTimeout(() => {
+      event.target.complete();
+      this.cardInAnimation();
+    }, 2000);
+  }
+  like() {
+    this.card.style.transform = 'translateY(100%) scale(0.5)';
+    this.cardInAnimation();
+  }
+  dislike() {
+    this.card.style.transform = 'translateX(-110%)';
+    this.cardInAnimation();
+  }
+  cardInAnimation() {
+    this.card.style.opacity = '0';
+    setTimeout(() => {
+      this.card.setAttribute(
+        'style',
+        'transform:translateX(110%); transition-duration:0s;'
+      );
+    }, 300);
+    setTimeout(() => {
+      this.card.setAttribute(
+        'style',
+        'transform:translateX(0); opacity:1; transition-duration:0.3s;'
+      );
+      this.selectedItem =
+        this.items[Math.floor(Math.random() * this.items.length)];
+      setTimeout(() => {
+        this.swiper?.update();
+        this.swiper?.slideTo(0, 0);
+      }, 50);
+    }, 350);
+  }
 }
