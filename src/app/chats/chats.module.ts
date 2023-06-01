ba380729f90065e74fbca98f78c82444d5aae025
Umut Kalayci <kalayciumut@windowslive.com>
2023-06-01 23:36:37 +0300

    feat: chats page added no chat message

--- src/app/chats/chats.module.ts
+++ src/app/chats/chats.module.ts
@@ -3,18 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChatsPage } from './chats.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { ChatsPageRoutingModule } from './chats-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    ChatsPageRoutingModule,
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, ChatsPageRoutingModule],
   declarations: [ChatsPage],
 })
 export class ChatsPageModule {}
