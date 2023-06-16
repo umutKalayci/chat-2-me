020679af1737567f11df53ac753c443cb3e91a8b
Umut Kalayci <kalayciumut@windowslive.com>
2023-06-16 05:05:14 +0300

    build: android build files

---
+++ android/app/src/main/java/com/chat2me/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.chat2me.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.chat2me.app',
   appName: 'chat-2-me',
   webDir: 'www',
   server: {
